d8584af20a39344849067f598085c3b57852ea98
Miguel Zhunio est <111152098+miguelZhunio@users.noreply.github.com>
2024-04-01 20:29:22 -0500

    Initial commit

---
+++ src/main/java/projects/java/ProjectsJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package projects.java;
+
+/**
+ *
+ * @author zhuniox
+ */
+public class ProjectsJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
